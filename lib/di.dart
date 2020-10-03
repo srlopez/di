91c67ab46385dd610f64927029a79ee4999a8538
santi <srlopezh@gmail.com>
2020-10-03 12:25:11 +0200

    Solucion Mejorada

--- lib/di.dart
+++ lib/di.dart
@@ -4,29 +4,32 @@ int calculate() {
   return 6 * 7;
 }
 
-
-class Vehicle{
+class Vehicle {
   final String _license;
   final String _description;
 
-  Vehicle({String license, String description}) : _license = license, _description = description;
+  Vehicle({String license, String description})
+      : _license = license,
+        _description = description;
 
   double Move(int km) {
     print('Moving vehicle: $this');
-    return Random().nextDouble()* km;
+    return Random().nextDouble() * km;
   }
 
   @override
   String toString() {
-      return 'Vehicle [license=$_license, description=$_description';
+    return 'Vehicle [license: $_license, description: $_description]';
   }
 }
 
-class Traveller{
+class Traveller {
   final String _name;
   final Vehicle _vehicle;
 
-  Traveller({String name, Vehicle vehicle}) : _name = name, _vehicle = vehicle;
+  Traveller({String name, Vehicle vehicle})
+      : _name = name,
+        _vehicle = vehicle;
 
   double Move(int km) {
     print('$_name travelling: $km');
