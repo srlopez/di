a9c7d451c01f4ebb629c050a7211cf3a7294cbfa
santi <srlopezh@gmail.com>
2020-10-03 16:46:48 +0200

    Solucion GetIt

--- bin/di.dart
+++ bin/di.dart
@@ -1,16 +1,23 @@
-import 'package:di/di.dart' as di;
-import '../lib/di.dart';
+import 'package:get_it/get_it.dart';
+
+import 'package:di/model.dart';
+import 'package:di/getit.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${di.calculate()}!');
+  var getIt = GetIt.instance;
 
-  var fabia = Car(license: '0249JSH', description: 'Skoda fabia');
-  var ave = Train(license: '16ESP', description: 'Ave M. de Cervantes');
+  // getIt.registerSingleton<di.Car>(
+  //     di.Car(license: '01234', description: 'Tesla model 3'));
+  // getIt.registerSingleton<Train>(
+  //     Train(license: 'z2ESP', description: 'Ave Fray Luis de León'));
+  init();
 
-  var tourist = Traveller(name: 'Aroa López', vehicle: fabia);
+  print('Hello world: ${calculate()}!');
+
+  var tourist = Traveller(name: 'Aroa López', vehicle: getIt.get<Car>());
   print('Moving! Cost: ${tourist.Move(15)}€');
   print('Moving! Cost: ${tourist.Move(1)}€');
 
-  tourist.vehicle = ave;
+  tourist.vehicle = getIt.get<Train>();
   print('Moving! Cost: ${tourist.Move(42)}€');
 }
