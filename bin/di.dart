11e683758f643c75e92b7440723b4d7b64cbdc9a
srlopez <you@example.com>
2020-10-04 10:39:03 +0200

    desacoplada

--- bin/di.dart
+++ bin/di.dart
@@ -4,13 +4,9 @@ import '../lib/di.dart';
 void main(List<String> arguments) {
   print('Hello world: ${di.calculate()}!');
 
-  var fabia = Car(license: '0249JSH', description: 'Skoda fabia');
-  var ave = Train(license: '16ESP', description: 'Ave M. de Cervantes');
-
+  var fabia = Vehicle(license: '0249JSH', description: 'Skoda fabia');
   var tourist = Traveller(name: 'Aroa López', vehicle: fabia);
   print('Moving! Cost: ${tourist.Move(15)}€');
   print('Moving! Cost: ${tourist.Move(1)}€');
-
-  tourist.vehicle = ave;
   print('Moving! Cost: ${tourist.Move(42)}€');
 }
