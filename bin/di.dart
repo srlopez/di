ed2ad686fcfe4b937e41e4fd17405d7e22c823fe
srlopez <you@example.com>
2020-10-04 10:41:56 +0200

    acoplada

--- bin/di.dart
+++ bin/di.dart
@@ -1,12 +1,12 @@
 import 'package:di/di.dart' as di;
+
 import '../lib/di.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${di.calculate()}!');
 
-  var fabia = Vehicle(license: '0249JSH', description: 'Skoda fabia');
-  var tourist = Traveller(name: 'Aroa López', vehicle: fabia);
-  print('Moving! Cost: ${tourist.Move(15)}€');
-  print('Moving! Cost: ${tourist.Move(1)}€');
-  print('Moving! Cost: ${tourist.Move(42)}€');
+  Traveller tourist = Traveller(name: "Jhon Smith");
+  print("Moving! Cost: ${tourist.Move(15)}€");
+  print("Moving! Cost: ${tourist.Move(1)}€");
+  print("Moving! Cost: ${tourist.Move(42)}€");
 }
